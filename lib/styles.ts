a0a4aac61b5b77d71e16b6aaed1359289e47821b
Alex Finley <alexfin@outlook.com>
2024-07-28 16:58:00 -0400

    icon and splash

--- lib/styles.ts
+++ lib/styles.ts
@@ -1,7 +1,11 @@
 /* COLORS */
 export const SILK_CHOCOLATE = "#6E5E63";
 export const OFF_WHITE = "#f9f1f1";
-export const YELLOW = "#E85F5C";
+export const PALE_YELLOW = "#FBECC0"; //#F2E699
 export const GRAY = "#2B4162";
 export const TAN_GRAY = "#BBA0B2";
-export const BACKGROUND_COLOR = "#3D4951";
+export const BACKGROUND_COLOR = "#152228";
+export const PIRATE_TAN ="#948767"
+/* FONTS */
+export const REGULAR_FONT = "Lusitana_400Regular"
+export const BOLD_FONT = "Lusitana_700Bold"
